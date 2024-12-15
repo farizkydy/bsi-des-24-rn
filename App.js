f7c0c2211bf7f7dfa33d90ea60e6830d17613158
Fariz <fariz.rizaldy@salt.co.id>
2024-12-15 21:48:25 +0700

    tab navigation done

--- App.js
+++ App.js
@@ -6,25 +6,35 @@ import LoginScreen from './screens/Login'
 
 
 import { NavigationContainer } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName='Login'
+    <Tab.Navigator
+    screenOptions={{
+      //   tabBarShowLabel: false, // Hide the label
+      tabBarStyle: { backgroundColor: "green" }, // set the background color
+      tabBarLabelPosition: "below-icon", // set the position of the label
+      tabBarActiveTintColor: "purple", // set the active tab color
+      tabBarInactiveTintColor: "white", // set the inactive tab color
+    }}
     >
-      <Stack.Screen 
+      <Tab.Screen 
         name="Home" 
         component={HomeScreen}
-      /> 
-      <Stack.Screen name="Login" component={LoginScreen} 
         options={{
-          headerShown: false // menghilangkan header
+          tabBarLabel: "Home Page", // set the label
+          tabBarIcon: () => <Ionicons name={"home"} size={20} />, // set the icon
+          tabBarBadge: 3, // set the badge
         }}
+      /> 
+      <Tab.Screen name="Login" component={LoginScreen} 
       />
-    </Stack.Navigator>
+    </Tab.Navigator>
   </NavigationContainer>
   );
 };
