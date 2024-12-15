8c579729b2990e210ca5a7788b1e6c7aee090046
Fariz <fariz.rizaldy@salt.co.id>
2024-12-15 22:01:14 +0700

    drawer done

--- App.js
+++ App.js
@@ -6,35 +6,36 @@ import LoginScreen from './screens/Login'
 
 
 import { NavigationContainer } from '@react-navigation/native'; 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Tab.Navigator
-    screenOptions={{
-      //   tabBarShowLabel: false, // Hide the label
-      tabBarStyle: { backgroundColor: "green" }, // set the background color
-      tabBarLabelPosition: "below-icon", // set the position of the label
-      tabBarActiveTintColor: "purple", // set the active tab color
-      tabBarInactiveTintColor: "white", // set the inactive tab color
-    }}
-    >
-      <Tab.Screen 
+    <Drawer.Navigator>
+      <Drawer.Screen 
         name="Home" 
         component={HomeScreen}
         options={{
-          tabBarLabel: "Home Page", // set the label
-          tabBarIcon: () => <Ionicons name={"home"} size={20} />, // set the icon
-          tabBarBadge: 3, // set the badge
+          title: "My dashboard", // set title
+          drawerLabel: "Dashboard label", // set label
+          drawerIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={22} color={color} />
+          ), // set icon
+          drawerActiveTintColor: "#333", // set active tab
+          drawerActiveBackgroundColor: "lightblue", // set active tab background color
+          drawerInactiveTintColor: "#666", // set inactive tab
+          drawerContentStyle: {
+            backgroundColor: "#c6cbef", // set drawer background color
+            paddingTop: 10, // set top padding
+          },
         }}
       /> 
-      <Tab.Screen name="Login" component={LoginScreen} 
+      <Drawer.Screen name="Login" component={LoginScreen} 
       />
-    </Tab.Navigator>
+    </Drawer.Navigator>
   </NavigationContainer>
   );
 };
